37dcd2d0f76d649b3be00ed9b5aaa1b6379cc858
Maria T. Andersen <mteilmann93@gmail.com>
2021-09-16 12:51:33 +0200

    modify the handleClick event of Board component. Update the value of stepNumber after a turn as a part of setState and update the definition of variable history to endable ability to time travel in game

--- src/index.js
+++ src/index.js
@@ -69,7 +69,7 @@ class Game extends React.Component {
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -80,6 +80,7 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
   }
