927c6cae45e189a542370766eb51a0fe856c8d13
Maria T. Andersen <mteilmann93@gmail.com>
2021-09-16 12:16:41 +0200

    move the handleClick method from the Board component to Game component and change the code according to the now changed structure of Game component's state

--- src/index.js
+++ src/index.js
@@ -20,18 +20,6 @@ We want to pass values from the parent Board component to the child Square compo
 Therefore we edit Board's renderSquare method to take a parameter i and set i to be the value of square.
 */
 class Board extends React.Component {
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
   renderSquare(i) {
     return (
       <Square
@@ -77,6 +65,22 @@ class Game extends React.Component {
       }],
       xIsNext: true,
     };
+  }
+
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+      }]),
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   render() {
